47b6325b8f9d93d74a85feb01cae1c2cd59fbde8
Thamilinpan-dot <tthamilinpan006@gmail.com>
2026-06-30 10:52:36 +0530

    Update WhileLoopText.java

--- WhileLoopText.java
+++ WhileLoopText.java
@@ -2,9 +2,9 @@ class WhileLoopText{
 	public static void main(String args[]){
 		int x =1;
 		while(x<=15) {
-		System.out.println("Institude Of Technology");
-		x++;
-	 }
+			System.out.println("Institude Of Technology");
+			x++;
+		}
 
 	}
 }
