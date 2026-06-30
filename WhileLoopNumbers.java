c592b6a431fda2888cec0a645881382b4860dd19
Thamilinpan-dot <tthamilinpan006@gmail.com>
2026-06-30 10:51:52 +0530

    Update WhileLoopNumbers.java

--- WhileLoopNumbers.java
+++ WhileLoopNumbers.java
@@ -2,9 +2,9 @@ class WhileLoopNumbers{
 	public static void main(String args[]){
 		int x =1;
 		while(x<=10) {
-		System.out.println(x);
-		x++;
-	 }
+			System.out.println(x);
+			x++;
+		}
 
 	}
 }
