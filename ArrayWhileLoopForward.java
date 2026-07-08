cd77ea4c05ad89d82c1ada15c7e8dd89e588c106
Thamilinpan-dot <tthamilinpan006@gmail.com>
2026-07-08 10:55:39 +0530

    Create ArrayWhileLoopForward.java

---
+++ ArrayWhileLoopForward.java
@@ -0,0 +1,13 @@
+class ArrayWhileLoopForward{
+	public static void main(String args[]){
+		int []x={10,20,30,40,50};
+		
+		int y=0;
+		while (y<x.length){
+			System.out.println(x[y]);
+			y++;
+			
+			
+		}
+	}
+}	
