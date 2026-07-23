c4061a8a20009490fbbfd567aaffe9b54bd0556f
Thamilinpan-dot <tthamilinpan006@gmail.com>
2026-07-23 14:36:16 +0530

    Create StarPattern1.java

---
+++ StarPattern1.java
@@ -0,0 +1,15 @@
+class StarPattern1 {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
