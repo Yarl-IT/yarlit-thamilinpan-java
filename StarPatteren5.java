0e0df68c34a2752dd7dfec0edadca7ae0580b626
Thamilinpan-dot <tthamilinpan006@gmail.com>
2026-08-06 10:15:12 +0530

    Update StarPatteren5.java

--- StarPatteren5.java
+++ StarPatteren5.java
@@ -1,19 +1,17 @@
 class StarPatteren5{
-    public static void main(String[] args){
-        int n = 5; 
+    public static void main(String args[]){
 
-        for (int i = 1; i <= n;  i++){
+        for (int i=1;i<=5;i++){
             
           
-            for (int j = i; j <n; j++){
+            for(int j=i;j<5;j++){
                 System.out.print("  "); 
             }
     
-            for (int k = 1; k <= (2*i-1); k++){
+            for(int k=1;k<=(2*i-1);k++){
                 System.out.print("* ");
             }
-            
-           
+			
             System.out.println();
         }
     }
